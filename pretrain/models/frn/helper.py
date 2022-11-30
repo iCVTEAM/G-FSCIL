dce37f5b32d4c578c1d8751b3320649a045c7b84
free_wind <53814998+zhao1f@users.noreply.github.com>
2022-11-30 23:54:09 +0800

    pretraining initial update

---
+++ pretrain/models/frn/helper.py
@@ -0,0 +1,176 @@
+# import new Network name here and add in model_class args
+from .Network import MYNET
+from utils import *
+from tqdm import tqdm
+import torch.nn.functional as F
+
+
+def base_train(model, trainloader, optimizer, scheduler, epoch, args):
+    tl = Averager()
+    ta = Averager()
+    model = model.train()
+    # standard classification for pretrain
+    tqdm_gen = tqdm(trainloader)
+    for i, batch in enumerate(tqdm_gen, 1):
+        data, train_label = [_.cuda() for _ in batch]
+
+        logits = model(data)
+        logits = logits[:, :args.base_class]
+        loss = F.cross_entropy(logits, train_label)
+        acc = count_acc(logits, train_label)
+
+        total_loss = loss
+
+        lrc = scheduler.get_last_lr()[0]
+        tqdm_gen.set_description(
+            'Session 0, epo {}, lrc={:.4f},total loss={:.4f} acc={:.4f}'.format(epoch, lrc, total_loss.item(), acc))
+        tl.add(total_loss.item())
+        ta.add(acc)
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+    tl = tl.item()
+    ta = ta.item()
+    return tl, ta
+
+
+def construct_protos(trainset, transform, model, args):
+    # replace fc.weight with the embedding average of train data
+    model = model.eval()
+
+    trainloader = torch.utils.data.DataLoader(dataset=trainset, batch_size=128,
+                                              num_workers=8, pin_memory=True, shuffle=False)
+    trainloader.dataset.transform = transform
+    embedding_list = []
+    label_list = []
+    # data_list=[]
+    with torch.no_grad():
+        for i, batch in enumerate(trainloader):
+            data, label = [_.cuda() for _ in batch]
+            model.module.mode = 'encoder'
+            embedding = model.module.get_feature_map(data) # N,HW,C
+
+            embedding_list.append(embedding.cpu())
+            label_list.append(label.cpu())
+    embedding_list = torch.cat(embedding_list, dim=0)
+    label_list = torch.cat(label_list, dim=0)
+
+    proto_list = []
+
+    for class_index in range(args.base_class):
+        data_index = (label_list == class_index).nonzero()
+        embedding_this = embedding_list[data_index.squeeze(-1)]
+        embedding_this = embedding_this.mean(0)
+        proto_list.append(embedding_this)
+
+    proto_list = torch.stack(proto_list, dim=0)
+    #(self.args.num_classes, self.resolution, self.num_features)
+    model.module.proto.data[:args.base_class] = proto_list
+    return model
+
+
+def replace_base_fc(trainset, transform, model, args):
+    # replace fc.weight with the embedding average of train data
+    model = model.eval()
+
+    trainloader = torch.utils.data.DataLoader(dataset=trainset, batch_size=128,
+                                              num_workers=8, pin_memory=True, shuffle=False)
+    trainloader.dataset.transform = transform
+    embedding_list = []
+    label_list = []
+    # data_list=[]
+    with torch.no_grad():
+        for i, batch in enumerate(trainloader):
+            data, label = [_.cuda() for _ in batch]
+            model.module.mode = 'encoder'
+            embedding = model(data)
+
+            embedding_list.append(embedding.cpu())
+            label_list.append(label.cpu())
+    embedding_list = torch.cat(embedding_list, dim=0)
+    label_list = torch.cat(label_list, dim=0)
+
+    proto_list = []
+
+    for class_index in range(args.base_class):
+        data_index = (label_list == class_index).nonzero()
+        embedding_this = embedding_list[data_index.squeeze(-1)]
+        embedding_this = embedding_this.mean(0)
+        proto_list.append(embedding_this)
+
+    proto_list = torch.stack(proto_list, dim=0)
+
+    model.module.fc.weight.data[:args.base_class] = proto_list
+
+    return model
+
+
+
+def test(model, testloader, epoch, args, session):
+    test_class = args.base_class + session * args.way
+
+
+    model = model.eval()
+    vl = Averager()
+    va_sum = Averager()
+    va_new = Averager()
+    va_base = Averager()
+    #if epoch ==0:
+    #    return vl, va_sum, va_base, va_new
+    if epoch %10 >0:
+        return vl.item(), va_sum.item(), va_base.item(), va_new.item()
+
+    base_classes = np.arange(0, args.base_class, 1)
+    new_classes = np.arange(args.base_class,args.base_class + session * args.way, 1)
+    with torch.no_grad():
+        tqdm_gen = tqdm(testloader)
+        for i, batch in enumerate(tqdm_gen, 1):
+            data, test_label = [_.cuda() for _ in batch]
+            logits = model(data,'normal')
+            logits = logits[:, :test_class]
+            loss = F.cross_entropy(logits, test_label)
+            acc = count_acc(logits, test_label)
+
+            base_correct_num, base_sum_num = count_acc_class(logits, test_label, base_classes)
+            va_base.num_add(base_sum_num)
+            va_base.acc_add(base_correct_num)
+
+            new_correct_num, new_sum_num = count_acc_class(logits, test_label, new_classes)
+            va_new.num_add(new_sum_num)
+            va_new.acc_add(new_correct_num)
+
+
+            vl.add(loss.item())
+            va_sum.add(acc)
+
+        vl = vl.item()
+        va_sum = va_sum.item()
+        va_base = va_base.calc()
+        va_new = va_new.calc()
+    print('epo {}, test, loss={:.4f} acc={:.4f}'.format(epoch, vl, va_sum))
+    print('epo {}, base_class, new_class={:.4f} acc={:.4f}'.format(epoch, va_base, va_new))
+    return vl, va_sum, va_base, va_new
+
+def test_old(model, testloader, epoch, args, session):
+    test_class = args.base_class + session * args.way
+    model = model.eval()
+    vl = Averager()
+    va = Averager()
+    with torch.no_grad():
+        tqdm_gen = tqdm(testloader)
+        for i, batch in enumerate(tqdm_gen, 1):
+            data, test_label = [_.cuda() for _ in batch]
+            logits = model(data)
+            logits = logits[:, :test_class]
+            loss = F.cross_entropy(logits, test_label)
+            acc = count_acc(logits, test_label)
+
+            vl.add(loss.item())
+            va.add(acc)
+
+        vl = vl.item()
+        va = va.item()
+    print('epo {}, test, loss={:.4f} acc={:.4f}'.format(epoch, vl, va))
+
+    return vl, va
